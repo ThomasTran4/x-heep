3a9c58817cf72d88941dc53069f9e632cd8a61f8
Thomas <thomas.tran@epfl.ch>
2025-05-24 17:46:42 +0200

    Modified comments

--- sw/applications/0_DOOM/doom/d_doomTop.c
+++ sw/applications/0_DOOM/doom/d_doomTop.c
@@ -570,8 +570,8 @@ void D_DoomLoop (void)
         if (!startedGame)
         {
             startedGame = true;
-            //G_DeferedInitNew(sk_hard, 1, 1);
-            D_AdvanceDemo(); 
+            G_DeferedInitNew(sk_medium, 1, 1);
+            //D_AdvanceDemo(); 
         }
         #endif
 
